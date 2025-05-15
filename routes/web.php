cef4d116d574fe981e6872767e500c1e51af4e6b
unknown <muamasy159@gmail.com>
2025-05-15 15:02:28 +0800

    Update login and web routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\StaffController;
 use App\Http\Controllers\DepartmentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 # Dashboard page
